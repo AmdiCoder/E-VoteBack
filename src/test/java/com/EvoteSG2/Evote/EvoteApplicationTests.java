55f3232785c21af73555692757b679ce4ec35ecc
amdycoder <amdyhm0@gmail.com>
2024-12-31 21:43:19 +0100

    ajout de fichier sprint pour le back-end

---
+++ src/test/java/com/EvoteSG2/Evote/EvoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EvoteSG2.Evote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
